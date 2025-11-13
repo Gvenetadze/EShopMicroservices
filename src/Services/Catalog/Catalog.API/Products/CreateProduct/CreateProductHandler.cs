4a32e02d69daa9766b1447a39e829a4b540b093f
Andria Gvenetadze <a.gvenetadze@andersenlab.com>
2025-11-13 11:46:42 +0400

    Add create product endpoints and CQRS

---
+++ src/Services/Catalog/Catalog.API/Products/CreateProduct/CreateProductHandler.cs
@@ -0,0 +1,25 @@
+﻿using BuildingBlocks.CQRS;
+using Catalog.API.Models;
+
+namespace Catalog.API.Products.CreateProduct;
+
+public record CreateProductCommand(string Name, List<string> Category, string ImageFile,
+    decimal Price, string Description) : ICommand<CreateProductResult>; 
+public record CreateProductResult(Guid Id);
+
+internal class CreateProductCommandHandler : ICommandHandler<CreateProductCommand, CreateProductResult>
+{
+    public async Task<CreateProductResult> Handle(CreateProductCommand command, CancellationToken cancellationToken)
+    {
+        var product = new Product
+        {
+            Name = command.Name,
+            Category = command.Category,
+            ImageFile = command.ImageFile,
+            Price = command.Price,
+            Description = command.Description
+        };
+
+        return new CreateProductResult(Guid.NewGuid());
+    }
+}
